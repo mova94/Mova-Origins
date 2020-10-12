ce06b3570e556eda27889e3a38478eb3b24f55f9
Nima Movasseghi <nimski94@gmail.com>
2020-10-12 00:29:01 -0500

    added auth check and register user

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 
 import { Navbar } from './components/layout/Navbar';
@@ -9,8 +9,19 @@ import Register  from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
 import store from './store';
+import setAuthToken from './utils/setAuthToken';
+import { loadUser } from './actions/auth';
+
+if (localStorage.token){
+  setAuthToken(localStorage.token)
+}
 
 const App = () => {
+  
+  useEffect(() => {
+    store.dispatch(loadUser());
+  },[]);
+
   return (
     <Provider store={store}>
       <Router>
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -3,8 +3,9 @@ import { Link } from 'react-router-dom'
 import {connect} from 'react-redux';
 import { setAlert } from './../../actions/alert';
 import PropTypes from 'prop-types';
+import { register } from './../../actions/auth';
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register }) => {
 
     const [formData, setFormData] = useState({
         name:'',
@@ -22,7 +23,11 @@ const Register = ({ setAlert }) => {
         if(password !== password2){
             setAlert('Passwords do not match', 'danger', 3000);
         } else{
-            console.log('Success')
+            register({
+                name,
+                email,
+                password
+            });
         }
     }
 
@@ -83,7 +88,8 @@ const Register = ({ setAlert }) => {
 }
 
 Register.propTypes = {
-    setAlert: PropTypes.PropTypes.func.isRequired
+    setAlert: PropTypes.func.isRequired,
+    register: PropTypes.func.isRequired
 };
 
-export default connect(null, { setAlert })(Register);
+export default connect(null, { setAlert, register })(Register);
