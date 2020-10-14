d8cda7e54c4b9a241ceccdf28f82897b21b51f27
Nima Movasseghi <mova@Nimas-MacBook-Pro.local>
2020-10-13 23:59:06 -0500

    added Private route for login/dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,8 @@ import Alert from './components/layout/Alert';
 import store from './store';
 import setAuthToken from './utils/setAuthToken';
 import { loadUser } from './actions/auth';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 
 if (localStorage.token){
   setAuthToken(localStorage.token)
@@ -33,6 +35,7 @@ const App = () => {
             <Switch>
               <Route exact path='/register' component={Register}/>
               <Route exact path='/login' component={Login}/>
+              <PrivateRoute exact path='/dashboard' component={Dashboard}/>
             </Switch>
           </section>
         </React.Fragment>    
