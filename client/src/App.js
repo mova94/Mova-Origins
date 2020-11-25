264cd99916e4b03c25443b2c124ea896d78f0f9a
Nima Movasseghi <nmovasseghi94@gmail.com>
2020-11-24 21:56:58 -0800

    Added post and posts structure

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,7 @@ import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/Profiles/Profiles';
 import Profile from './components/Profile/Profile';
+import Posts from './components/posts/Posts';
 
 if (localStorage.token){
   setAuthToken(localStorage.token)
@@ -48,6 +49,7 @@ const App = () => {
               <PrivateRoute exact path='/edit-profile' component={EditProfile}/>
               <PrivateRoute exact path='/add-experience' component={AddExperience}/>
               <PrivateRoute exact path='/add-education' component={AddEducation}/>
+              <PrivateRoute exact path='/posts' component={Posts}/>
             </Switch>
           </section>
         </React.Fragment>    
