b4015303ab22238b8c9bf6f1a9884797af910446
Nima Movasseghi <nmovasseghi94@gmail.com>
2020-11-25 17:38:14 -0800

    heroku setup

--- server/index.js
+++ server/index.js
@@ -19,7 +19,17 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-const port = 4000;
+//Serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+    //Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
     console.log(`Listening on port: ${port} .....`);
 })
